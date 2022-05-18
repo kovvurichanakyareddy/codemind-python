2940b6dcf03e4cf4651958fd2c488360167991ed
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-05-18 21:58:40 +0530

    n,k=map(int,input().split()) c=0 l=list(map(str,input().strip().split())) for i in range(n):     s=len(l[i])     if int(l[i])<0:         s-=1     if s==k:         c+=1
    print(c)

---
+++ K_digit_length_numbers_in_a_array.py
@@ -0,0 +1,11 @@
+n,k=map(int,input().split())
+c=0
+l=list(map(str,input().strip().split()))
+for i in range(n):
+    s=len(l[i])
+    if int(l[i])<0:
+        s-=1
+    if s==k:
+        c+=1
+    
+print(c)
