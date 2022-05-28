fae76a21d4b780e7a9103ec0bf026d1d0f717616
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-05-28 20:02:23 +0530

    nt=int(input()) for lo in range(nt):     a,b=map(int,input().split())     l1=list(map(int,input().split()))     l2=list(map(int,input().split()))     l=l1+l2     l.sort()     print(*l)

---
+++ Merge_Without_Extra_Space.py
@@ -0,0 +1,8 @@
+nt=int(input())
+for lo in range(nt):
+    a,b=map(int,input().split())
+    l1=list(map(int,input().split()))
+    l2=list(map(int,input().split()))
+    l=l1+l2
+    l.sort()
+    print(*l)
