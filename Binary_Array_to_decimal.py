0a0e6b8769df75b9618e6daf5f2e2248aedc32f1
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-07-02 11:15:53 +0530

    n=int(input()) t=0 c=0 l=list(map(int,input().split())) for i in range(n-1,-1,-1):     c+=(2**t)*l[i]     t+=1 print(c)

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -1,15 +1,8 @@
-def binary(n):
-    r=0
-    i=0
-    while n:
-        rem=n%10
-        r+=((2**i)*rem)
-        n//=10
-        i+=1
-    return r
 n=int(input())
-l=list(map(int,input().strip().split()))
-num=0
-for i in range(n):
-    num=num*10+l[i]
-print(binary(num))
+t=0
+c=0
+l=list(map(int,input().split()))
+for i in range(n-1,-1,-1):
+    c+=(2**t)*l[i]
+    t+=1
+print(c)
