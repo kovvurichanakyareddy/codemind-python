3480177a43d8b959a47c62b2c784c63e4f30975e
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-07-02 11:24:02 +0530

    n,k=map(int,input().split()) s=0 l=list(map(str,input().split())) for i in l:     c=0     for char in i:         if char!='-':             c+=1     if c==k:         s+=1 print(s)

--- K_digit_length_numbers_in_a_array.py
+++ K_digit_length_numbers_in_a_array.py
@@ -1,11 +1,11 @@
 n,k=map(int,input().split())
-c=0
-l=list(map(str,input().strip().split()))
-for i in range(n):
-    s=len(l[i])
-    if int(l[i])<0:
-        s-=1
-    if s==k:
-        c+=1
-    
-print(c)
+s=0
+l=list(map(str,input().split()))
+for i in l:
+    c=0
+    for char in i:
+        if char!='-':
+            c+=1
+    if c==k:
+        s+=1
+print(s)       
