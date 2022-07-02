6197587406d4d282762d85e94bd40b33ce2c24e0
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-07-02 10:59:20 +0530

    n=int(input()) arr=[] l=list(map(int,input().split())) for i in range(n):     c=0     for j in range(n):         if l[i]==l[j]:             c+=1     if c==l[i]:         arr.append(l[i])         l[i]=0 if len(arr)!=0:     print(min(arr),max(arr)) else:     print(-1)

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -1,19 +1,15 @@
 n=int(input())
-l=list(map(int,input().strip().split()))
-mi,ma,c=9,0,0
+arr=[]
+l=list(map(int,input().split()))
 for i in range(n):
-    s=0
+    c=0
     for j in range(n):
         if l[i]==l[j]:
-            s+=1
-    if s==l[i]:
-        c+=1
-        if l[i]<mi:
-            mi=l[i]
-        if l[i]>ma:
-            ma=l[i]
+            c+=1
+    if c==l[i]:
+        arr.append(l[i])
         l[i]=0
-if c>0:
-    print("%d %d"%(mi,ma))
+if len(arr)!=0:
+    print(min(arr),max(arr))
 else:
-    print('-1')
+    print(-1)
